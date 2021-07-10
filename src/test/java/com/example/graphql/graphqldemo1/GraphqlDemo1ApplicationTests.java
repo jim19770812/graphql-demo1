cee629c3ba77e95ef5448b7a4e059265991fb060
韩晓峰 <xiaofeng.han@inrevo.io>
2021-07-10 23:24:03 +0800

    init

---
+++ src/test/java/com/example/graphql/graphqldemo1/GraphqlDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphql.graphqldemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
